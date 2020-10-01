eef0c46e9e5f459de68744983e7a5a14b305c6f3
georgi-dimov-781 <george_kj@abv.bg>
2020-10-01 13:44:06 +0300

    First commit

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,9 +1,5 @@
 // Easter Egg to toggle color of light bulb picture once resume is clicked
 
-function activateFilterAnimation() {
-    $('#bwBulb').toggleClass('animated');
-  }
-  
-  $('#resume').on('click', activateFilterAnimation);
+
 
   
